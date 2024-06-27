c24418ab0ebdcafed92111b15046df8e8fe47b5c
oscarromero583 <obetaoscar583@gmail.com>
2024-06-27 05:07:36 +0100

    i an avin some errors from tis component but let me just sleep first. my head wan burst 	modified:   src/features.js

--- src/features.js
+++ src/features.js
@@ -11,7 +11,7 @@ interface FeatureProps extends StackProps {
     icon:ElementType;
 }
 
-function Feature({ icon, children, ...rest }: FeatureProps) {
+function Feature({ icon, children, ...rest }: FeatureProps) {  
     return (
       <HStack {...rest} spacing={'24px'}>
         <Icon as={icon} boxSize={'48px'} />
@@ -29,6 +29,7 @@ function Feature({ icon, children, ...rest }: FeatureProps) {
       <HStack>
         <Feature icon={StarTriangleIcon}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum?
+
         </Feature>
         <Feature icon={StarredTriangleIcon}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit.
